589381e9cb492a417602681ac47fb433bed80948
NwotYknuf <jlafille@ensc.fr>
2019-03-08 10:49:52 +0100

    Créer UC_AjoutDiplome

---
+++ UserControler/testUC/UC_AjoutDiplome.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dotnet.UserControler.testUC
+{
+    public partial class UC_AjoutDiplome : UC_Ajout
+    {
+        public UC_AjoutDiplome()
+        {
+            InitializeComponent();
+            this.groupBox.Text = "Ajouter un Diplome";
+        }
+
+        private void bCreer_Click(object sender, EventArgs e)
+        {
+            //ajouter un 
+        }
+    }
+}
