a894fc6f73f8cca8e4b839b87e6ae0556d2b501d
NwotYknuf <jlafille@ensc.fr>
2019-03-08 11:30:29 +0100

    UC pour le fil d'arianne

---
+++ UserControler/FilArianne/UC_FilArianne.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dotnet{
+    public partial class FilArianne : UserControl {
+        public FilArianne() {
+
+            InitializeComponent();
+
+            for(int i = 0; i < 5; i++) {
+                LinkLabel l = new LinkLabel();
+                l.Text = "toast";
+                flowLayoutPanel.Controls.Add(l);
+            }
+        }
+    }
+}
