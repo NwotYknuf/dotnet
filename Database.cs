ebca52d484d7ad3b05578c9c2296ac2844e8a4b8
NwotYknuf <jlafille@ensc.fr>
2019-03-08 11:08:56 +0100

    Singloton pour la base de donnés

---
+++ Database.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet
+{
+    public class Database : sysEntities
+    {
+        private static Database _instance = null;
+
+        public static Database instance {
+            get {
+                if (_instance == null)
+                {
+                    _instance = new Database();
+                }
+                return _instance;
+            }
+        }
+
+        private Database() : base() { }
+        
+    }
+}
