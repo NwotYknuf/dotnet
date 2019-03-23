614812a546ab8ef1232bfd32328ea6b028cb0925
Snivell <vincent.gindt@gmail.com>
2019-03-23 15:42:40 +0100

    onglet accueil

--- UserControler/UC_OngletAccueil.Designer.cs
+++ UserControler/UC_OngletAccueil.Designer.cs
@@ -28,33 +28,210 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.lBienvenu = new System.Windows.Forms.Label();
+            this.pBienvenu = new System.Windows.Forms.Panel();
+            this.pAccueil = new System.Windows.Forms.Panel();
+            this.lHorloge = new System.Windows.Forms.Label();
+            this.timerBienvenu = new System.Windows.Forms.Timer(this.components);
+            this.timerHorloge = new System.Windows.Forms.Timer(this.components);
+            this.lDate = new System.Windows.Forms.Label();
+            this.pAffPersonnel = new System.Windows.Forms.Panel();
+            this.pAffCours = new System.Windows.Forms.Panel();
+            this.gBAffPersonnel = new System.Windows.Forms.GroupBox();
+            this.gBAffCours = new System.Windows.Forms.GroupBox();
+            this.fLPAffPersonnel = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.lElement1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pBienvenu.SuspendLayout();
+            this.pAccueil.SuspendLayout();
+            this.pAffPersonnel.SuspendLayout();
+            this.pAffCours.SuspendLayout();
+            this.gBAffPersonnel.SuspendLayout();
+            this.gBAffCours.SuspendLayout();
+            this.fLPAffPersonnel.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // lBienvenu
             // 
+            this.lBienvenu.AutoEllipsis = true;
             this.lBienvenu.AutoSize = true;
-            this.lBienvenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lBienvenu.Location = new System.Drawing.Point(361, 324);
+            this.lBienvenu.Font = new System.Drawing.Font("Pristina", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lBienvenu.ForeColor = System.Drawing.Color.White;
+            this.lBienvenu.Location = new System.Drawing.Point(143, 194);
             this.lBienvenu.Name = "lBienvenu";
-            this.lBienvenu.Size = new System.Drawing.Size(382, 24);
+            this.lBienvenu.Size = new System.Drawing.Size(858, 252);
             this.lBienvenu.TabIndex = 0;
-            this.lBienvenu.Text = "Bienvenu dans notre application de gestion !";
+            this.lBienvenu.Text = "Bienvenu dans notre \r\n      application de gestion !";
             // 
-            // UCAccueil
+            // pBienvenu
+            // 
+            this.pBienvenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(185)))), ((int)(((byte)(213)))));
+            this.pBienvenu.Controls.Add(this.lBienvenu);
+            this.pBienvenu.Location = new System.Drawing.Point(3, 3);
+            this.pBienvenu.Name = "pBienvenu";
+            this.pBienvenu.Size = new System.Drawing.Size(1106, 676);
+            this.pBienvenu.TabIndex = 1;
+            this.pBienvenu.Click += new System.EventHandler(this.pBienvenu_Click);
+            // 
+            // pAccueil
+            // 
+            this.pAccueil.Controls.Add(this.pAffCours);
+            this.pAccueil.Controls.Add(this.pAffPersonnel);
+            this.pAccueil.Controls.Add(this.lDate);
+            this.pAccueil.Controls.Add(this.lHorloge);
+            this.pAccueil.Location = new System.Drawing.Point(3, 3);
+            this.pAccueil.Name = "pAccueil";
+            this.pAccueil.Size = new System.Drawing.Size(1106, 676);
+            this.pAccueil.TabIndex = 1;
+            // 
+            // lHorloge
+            // 
+            this.lHorloge.AutoSize = true;
+            this.lHorloge.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lHorloge.Location = new System.Drawing.Point(930, 58);
+            this.lHorloge.Name = "lHorloge";
+            this.lHorloge.Size = new System.Drawing.Size(96, 25);
+            this.lHorloge.TabIndex = 0;
+            this.lHorloge.Text = "hh:mm:ss";
+            // 
+            // timerBienvenu
+            // 
+            this.timerBienvenu.Tick += new System.EventHandler(this.timerBienvenu_Tick);
+            // 
+            // timerHorloge
+            // 
+            this.timerHorloge.Tick += new System.EventHandler(this.timerHorloge_Tick);
+            // 
+            // lDate
+            // 
+            this.lDate.AutoSize = true;
+            this.lDate.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lDate.Location = new System.Drawing.Point(829, 18);
+            this.lDate.Name = "lDate";
+            this.lDate.Size = new System.Drawing.Size(249, 31);
+            this.lDate.TabIndex = 1;
+            this.lDate.Text = "Jour Num Mois Année";
+            // 
+            // pAffPersonnel
+            // 
+            this.pAffPersonnel.Controls.Add(this.gBAffPersonnel);
+            this.pAffPersonnel.Location = new System.Drawing.Point(3, 96);
+            this.pAffPersonnel.Name = "pAffPersonnel";
+            this.pAffPersonnel.Size = new System.Drawing.Size(550, 577);
+            this.pAffPersonnel.TabIndex = 2;
+            // 
+            // pAffCours
+            // 
+            this.pAffCours.Controls.Add(this.gBAffCours);
+            this.pAffCours.Location = new System.Drawing.Point(553, 96);
+            this.pAffCours.Name = "pAffCours";
+            this.pAffCours.Size = new System.Drawing.Size(550, 577);
+            this.pAffCours.TabIndex = 3;
+            // 
+            // gBAffPersonnel
+            // 
+            this.gBAffPersonnel.Controls.Add(this.fLPAffPersonnel);
+            this.gBAffPersonnel.Location = new System.Drawing.Point(15, 12);
+            this.gBAffPersonnel.Name = "gBAffPersonnel";
+            this.gBAffPersonnel.Size = new System.Drawing.Size(519, 550);
+            this.gBAffPersonnel.TabIndex = 0;
+            this.gBAffPersonnel.TabStop = false;
+            this.gBAffPersonnel.Text = "Les personnels qui n\'ont pas toutes leurs heures affectées : ";
+            // 
+            // gBAffCours
+            // 
+            this.gBAffCours.Controls.Add(this.flowLayoutPanel1);
+            this.gBAffCours.Location = new System.Drawing.Point(16, 13);
+            this.gBAffCours.Name = "gBAffCours";
+            this.gBAffCours.Size = new System.Drawing.Size(519, 550);
+            this.gBAffCours.TabIndex = 1;
+            this.gBAffCours.TabStop = false;
+            this.gBAffCours.Text = "Les cours non affectés : ";
+            // 
+            // fLPAffPersonnel
+            // 
+            this.fLPAffPersonnel.Controls.Add(this.lElement1);
+            this.fLPAffPersonnel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.fLPAffPersonnel.Location = new System.Drawing.Point(6, 19);
+            this.fLPAffPersonnel.Name = "fLPAffPersonnel";
+            this.fLPAffPersonnel.Padding = new System.Windows.Forms.Padding(10);
+            this.fLPAffPersonnel.Size = new System.Drawing.Size(507, 525);
+            this.fLPAffPersonnel.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(6, 18);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(10);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(507, 525);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // lElement1
+            // 
+            this.lElement1.AutoSize = true;
+            this.lElement1.Location = new System.Drawing.Point(13, 10);
+            this.lElement1.Name = "lElement1";
+            this.lElement1.Size = new System.Drawing.Size(88, 13);
+            this.lElement1.TabIndex = 0;
+            this.lElement1.Text = "Tout est en ordre";
+            this.lElement1.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tout est en ordre";
+            this.label1.Visible = false;
+            // 
+            // UC_OngletAccueil
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.lBienvenu);
-            this.Name = "UCAccueil";
+            this.Controls.Add(this.pAccueil);
+            this.Controls.Add(this.pBienvenu);
+            this.Name = "UC_OngletAccueil";
             this.Size = new System.Drawing.Size(1112, 682);
+            this.Load += new System.EventHandler(this.UC_OngletAccueil_Load);
+            this.pBienvenu.ResumeLayout(false);
+            this.pBienvenu.PerformLayout();
+            this.pAccueil.ResumeLayout(false);
+            this.pAccueil.PerformLayout();
+            this.pAffPersonnel.ResumeLayout(false);
+            this.pAffCours.ResumeLayout(false);
+            this.gBAffPersonnel.ResumeLayout(false);
+            this.gBAffCours.ResumeLayout(false);
+            this.fLPAffPersonnel.ResumeLayout(false);
+            this.fLPAffPersonnel.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Label lBienvenu;
+        private System.Windows.Forms.Panel pBienvenu;
+        private System.Windows.Forms.Panel pAccueil;
+        private System.Windows.Forms.Timer timerBienvenu;
+        private System.Windows.Forms.Label lHorloge;
+        private System.Windows.Forms.Timer timerHorloge;
+        private System.Windows.Forms.Label lDate;
+        private System.Windows.Forms.Panel pAffCours;
+        private System.Windows.Forms.GroupBox gBAffCours;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel pAffPersonnel;
+        private System.Windows.Forms.GroupBox gBAffPersonnel;
+        private System.Windows.Forms.FlowLayoutPanel fLPAffPersonnel;
+        private System.Windows.Forms.Label lElement1;
     }
 }
--- UserControler/UC_OngletAccueil.cs
+++ UserControler/UC_OngletAccueil.cs
@@ -16,5 +16,88 @@ namespace dotnet
         {
             InitializeComponent();
         }
+
+        private void UC_OngletAccueil_Load(object sender, EventArgs e)
+        {
+            timerBienvenu.Interval = 3000; // 3 secondes
+            timerBienvenu.Enabled = true;
+            timerBienvenu.Start();
+
+            timerHorloge.Interval = 1000; // 1 seconde
+            timerHorloge.Enabled = true;
+            timerHorloge.Start();
+
+            pBienvenu.BringToFront();
+        }
+
+        //Après 3 secondes, fais disparaitre le panel de Bienvenu
+        private void timerBienvenu_Tick(object sender, EventArgs e)
+        {
+            for(int i = 100; i >= 0; i--)
+            {
+                pBienvenu.BackColor = Color.FromArgb(i, 60, 185, 213);
+                pBienvenu.Update();
+                
+            }
+            pBienvenu.Visible = false;
+            timerBienvenu.Stop();
+        }
+
+        //Affiche toutes les secondes l'heure à l'horloge
+        private void timerHorloge_Tick(object sender, EventArgs e)
+        {
+            String jour = DateTime.Now.DayOfWeek.ToString();
+            int numero = DateTime.Now.Day;
+            int mois = DateTime.Now.Month;
+            int annee = DateTime.Now.Year;
+
+            int heure = DateTime.Now.Hour;
+            int minute = DateTime.Now.Minute;
+            int seconde = DateTime.Now.Second;
+
+            lDate.Text = convertitJour(jour) + " " + numero.ToString() + " " + convertitMois(mois) + " " + annee.ToString();
+            lHorloge.Text = heure.ToString() + ":" + minute.ToString() + ":" + seconde.ToString();
+        }
+
+        private String convertitJour(String jour)
+        {
+            switch(jour)
+            {
+                case "Monday": return "Lundi";
+                case "Tuesday": return "Mardi";
+                case "Wednesday": return "Mercredi";
+                case "Thursday": return "Jeudi";
+                case "Friday": return "Vendredi";
+                case "Saturday": return "Samedi";
+                case "Sunday": return "Dimanche";
+                default: return "";
+            }
+        }
+
+        private String convertitMois(int mois)
+        {
+            switch (mois)
+            {
+                case 1: return "Janvier";
+                case 2: return "Février";
+                case 3: return "Mars";
+                case 4: return "Avril";
+                case 5: return "Mai";
+                case 6: return "Juin";
+                case 7: return "Juillet";
+                case 8: return "Août";
+                case 9: return "Septembre";
+                case 10: return "Octobre";
+                case 11: return "Novembre";
+                case 12: return "Décembre";
+                default: return "";
+            }
+        }
+
+        private void pBienvenu_Click(object sender, EventArgs e)
+        {
+            pBienvenu.Visible = false;
+            timerBienvenu.Stop();
+        }
     }
 }
