d299e532ed6a128e5ec9831aed29985d9d8fec4b
Snivell <vincent.gindt@gmail.com>
2019-04-14 16:07:45 +0200

    petit ajout de couleur sur le menu

--- FenetrePrincipale.cs
+++ FenetrePrincipale.cs
@@ -26,26 +26,40 @@ namespace dotnet
         private void miAcceuil_Click(object sender, EventArgs e)
         {
             ucAccueil1.BringToFront();
+            changerCouleurDeFond(miAcceuil);
         }
 
         private void miDiplome_Click(object sender, EventArgs e)
         {
             ucListeDiplome1.BringToFront();
+            changerCouleurDeFond(miDiplome);
         }
 
         private void miPersonnel_Click(object sender, EventArgs e)
         {
             ucListePersonnel1.BringToFront();
+            changerCouleurDeFond(miPersonnel);
         }
 
         private void miGestion_Click(object sender, EventArgs e)
         {
             ucGestion1.BringToFront();
+            changerCouleurDeFond(miGestion);
         }
 
         private void miRecherche_Click(object sender, EventArgs e)
         {
             ucRecherche1.BringToFront();
+            changerCouleurDeFond(miRecherche);
+        }
+
+        private void changerCouleurDeFond(ToolStripMenuItem ms)
+        {
+            foreach(ToolStripMenuItem mItem in msMenu.Items)
+            {
+                mItem.BackColor = Color.FromArgb(60,185,213);
+            }
+            ms.BackColor = Color.FromArgb(20, 79, 93);
         }
     }
 }
