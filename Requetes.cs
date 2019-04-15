a7c18517ebabe2c5a723e4bc3e708279aba57504
NwotYknuf <jlafille@ensc.fr>
2019-04-15 11:14:23 +0200

    Requete pour le nombre d'heures attribuées

---
+++ Requetes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet {
+    public static class Requetes {
+        
+        public static List<cours> coursAttribues(personnel personnel) {
+            var coursAttribues = Database.instance.cours.Where(x => x.personnel.id == personnel.id).ToList();
+            List<cours> liste = new List<cours>();
+            foreach(cours c in coursAttribues) {
+                liste.Add(c);
+            }
+
+            return liste;
+        }
+
+        public static int nbrHeureAttribuees(personnel personnel) {
+            int res = 0;
+            foreach(cours c in coursAttribues(personnel)) {
+                res += (int)c.volume_horraire;
+            }
+            return res;
+        }
+
+
+    }
+}
