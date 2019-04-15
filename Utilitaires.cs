69a97a673b810e77c68a96b4adfdd3892f1ffa22
Snivell <vincent.gindt@gmail.com>
2019-04-15 17:43:16 +0200

    Ajout d'une classe utilitaire

---
+++ Utilitaires.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dotnet
+{
+    class Utilitaires
+    {
+
+        public static bool conditionsRespectees(String champsAControler, bool accepteLesLettres, bool accepteLesChiffres, bool accepteLesEspaces, bool accepteLesCaracteresSpeciaux, int nbCaracteresMin, int nbCaracteresMax)
+        {
+            // Renvoie vrai si le champsAControler respecte une taille comprise entre nbCaractereMin et nbCaractereMax et les caractères souhaitées en entrés
+
+            String reg = "^["; // ^ : Début de ligne, [ : début du conténaire
+            if (accepteLesLettres) reg += "a-zA-Z"; // a - z : Tous les caractères minuscules, A-Z : Tous les caractères majuscules
+            if (accepteLesChiffres) reg += "0-9"; // 0-9 : Tous les chiffres
+            if (accepteLesEspaces) reg += "\\s"; // \s : Tous les caractères espaces
+            if (accepteLesCaracteresSpeciaux) reg += "\\W"; // Tous les carcatères spéciaux
+            reg += "]+$"; // + : Un à plusieurs caractères, $ : Fin de ligne
+
+            Regex monRegex = new Regex(reg);
+
+            if ((champsAControler.Length >= nbCaracteresMin) && (champsAControler.Length <= nbCaracteresMax) && (monRegex.IsMatch(champsAControler)))
+                return true;
+            else
+                return false;
+
+        }
+
+        public static bool conditionsRespectees(String champAControler, ComboBox cb)
+        {
+            // Renvoie vrai si le champsAControler fait parti des champs de la combobox, faux sinon
+
+            if (cb.Items.IndexOf(champAControler) != -1)
+                return true;
+            return false;
+        }
+
+    }
+}
