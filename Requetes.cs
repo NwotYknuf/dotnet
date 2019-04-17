f648ef172588e2bf1202bfc0fe49b232e7feb276
Snivell <vincent.gindt@gmail.com>
2019-04-17 11:05:33 +0200

    Ajout de nombreuses requetes sur les cours et personnels

--- Requetes.cs
+++ Requetes.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 namespace dotnet {
     public static class Requetes {
         
+        // Requêtes spécifiques à un personnel
+
         public static List<cours> coursAttribues(personnel personnel) {
             return Database.instance.cours.Where(x => x.personnel.id == personnel.id).ToList<cours>();
         }
@@ -25,6 +27,33 @@ namespace dotnet {
 
         public static int nbrHeureManquantes(personnel personnel) {
             return nbrHeureDues(personnel) - nbrHeureAttribuees(personnel);
+        }
+
+        public static categorie obtientCategorieduPersonnel(personnel p)
+        {
+            return Database.instance.categorie.Where(x => x.id == p.id_categorie).FirstOrDefault<categorie>();
+        }
+
+        // Requêtes spécifiques à un cours :
+
+        public static ec obtientECduCours(cours c)
+        {
+            return Database.instance.ec.Where(x => x.id == c.id_ec).FirstOrDefault<ec>();
+        }
+
+        public static type_cours obtientTypeduCours(cours c)
+        {
+            return Database.instance.type_cours.Where(x => x.id == c.id_type).FirstOrDefault<type_cours>();
+        }
+
+        public static personnel obtientPersonnelduCours(cours c)
+        {
+            return Database.instance.personnel.Where(x => x.id == c.personnel.id).FirstOrDefault<personnel>();
+        }
+
+        public static void enregistreLaBDD()
+        {
+            Database.instance.SaveChanges();
         }
 
 
